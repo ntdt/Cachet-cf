fab4c52130402cf457a7ab958ead694c89fef3ac
Tho Duy Nguyen <tho.duy.nguyen@desjardins.com>
2017-07-25 10:35:40 -0400

    fix sed with timezone contains character / in timezone

--- ci/tasks/prepare-app-manifest.sh
+++ ci/tasks/prepare-app-manifest.sh
@@ -8,7 +8,7 @@ s/app_admin_username/${app_admin_username}/; \
 s/app_admin_password/${app_admin_password}/; \
 s/app_admin_email/${app_admin_email}/g; \
 s/app_admin_api_key/${app_admin_api_key}/; \
-s/app_timezone/${app_timezone}/; \
+s:app_timezone:${app_timezone}:; \
 s/app_locale/${app_locale}/; \
 s/app_mail_address/${app_mail_address}/;" cachet-cf/ci/templates/manifest.yml > ./app-manifest-output/manifest.yml
 echo "Done."
