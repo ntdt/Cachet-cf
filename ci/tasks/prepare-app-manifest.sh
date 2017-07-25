1eb094fd06cf730aaf5de550f4be9e143e2fd41f
Tho Duy Nguyen <tho.duy.nguyen@desjardins.com>
2017-07-25 10:26:21 -0400

    fix variable name app_timezone

--- ci/tasks/prepare-app-manifest.sh
+++ ci/tasks/prepare-app-manifest.sh
@@ -8,9 +8,7 @@ s/app_admin_username/${app_admin_username}/; \
 s/app_admin_password/${app_admin_password}/; \
 s/app_admin_email/${app_admin_email}/g; \
 s/app_admin_api_key/${app_admin_api_key}/; \
-s/app_timezone/${app_timezore}/; \
+s/app_timezone/${app_timezone}/; \
 s/app_locale/${app_locale}/; \
 s/app_mail_address/${app_mail_address}/;" cachet-cf/ci/templates/manifest.yml > ./app-manifest-output/manifest.yml
 echo "Done."
-echo "Unzip cachet-release/cachet-cf-bundled.zip to cachet-cf-app"
-unzip cachet-release/cachet-cf-bundled.zip -d cachet-cf-app
