562d754472c98135bbe563970a7253c4de65465f
Tho Duy Nguyen <tho.duy.nguyen@desjardins.com>
2017-07-26 17:26:54 -0400

    remove unused sudo

--- ci/tasks/update-pcf-components.sh
+++ ci/tasks/update-pcf-components.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-sudo cp tool-om/om-linux /usr/local/bin
-sudo chmod 755 /usr/local/bin/om-linux
+cp tool-om/om-linux /usr/local/bin
+chmod 755 /usr/local/bin/om-linux
 
 echo "=============================================================================================="
 echo " Get deployed products @ https://opsman.$pcf_ert_domain ..."
