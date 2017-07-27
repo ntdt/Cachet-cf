c159ef7f234d5a00ed13f321b14a49c2c02f1eae
Tho Duy Nguyen <tho.duy.nguyen@desjardins.com>
2017-07-27 14:17:16 -0400

    fix variable name

--- ci/tasks/update-pcf-components.rb
+++ ci/tasks/update-pcf-components.rb
@@ -28,7 +28,7 @@ def update_component_version(api_url, components, component_id, description, ver
   request = Net::HTTP::Put.new(url)
   request['Content-Type'] = 'application/json'
   request['X-Cachet-Token'] = "#{$api_token}"
-  name = "#{components[description]} #{version}"
+  name = "#{components['description']} #{version}"
   puts name
   request.body = "{\"name\":\"#{name}\", \"status\":1}"
   response = http.request(request)
@@ -62,16 +62,10 @@ pcf_components    = ENV['pcf_components']
 api_token         = ENV['app_admin_api_key']
 pcf_ert_domain    = ENV['pcf_ert_domain']
 pcf_opsman_admin  = ENV['pcf_opsman_admin']
-pcf_opsman_passwd = ENV['pcf_opsman_passwd']
+pcf_opsman_passwd = ENV['pcf_opsman_admin_passwd']
 api_endpoint      = "https://#{ENV['app_name']}.apps.#{pcf_ert_domain}/api/v1"
 opsman_url        = "https://opsman.#{pcf_ert_domain}"
-components        = {"cf"         => "Pivotal Elastic Runtime",
-                     "p-mysql"    => "MySQL for PCF",
-                     "p-redis"    => "Redis for PCF",
-                     "p-rabbitmq" => "RabbitMQ for PCF",
-                     "apm"        => "PCF Metrics",
-                     "p-spring-cloud-services"        => "Spring Cloud Services",
-                     "Pivotal_Single_Sign-On_Service" => "Single Sign-On"}
+
 tiles_versions = get_deployed_products(opsman_url, pcf_opsman_admin, pcf_opsman_passwd)
 tiles_versions.each do |tile, version|
   tile_id = get_component_id_from_description(api_endpoint, api_token, tile)
