4cef20265335a10c9e3d50eda26f25f6c43c8a69
Tho Duy Nguyen <tho.duy.nguyen@desjardins.com>
2017-07-27 14:35:02 -0400

    remove unused pcf_components in update-pcf-components

--- ci/tasks/update-pcf-components.rb
+++ ci/tasks/update-pcf-components.rb
@@ -21,14 +21,14 @@ def get_component_id_from_description(api_url, api_token, description)
   return 0
 end
 
-def update_component_version(api_url, components, component_id, description, version)
+def update_component_version(api_url, component_id, description, version)
   url = URI("#{api_url}/components/#{component_id}")
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = true
   request = Net::HTTP::Put.new(url)
   request['Content-Type'] = 'application/json'
   request['X-Cachet-Token'] = "#{$api_token}"
-  name = "#{components['description']} #{version}"
+  name = "Update tile #{description}  to #{version}"
   puts name
   request.body = "{\"name\":\"#{name}\", \"status\":1}"
   response = http.request(request)
@@ -58,7 +58,6 @@ def get_deployed_products(opsman_url,user, password)
   end
 end
 
-pcf_components    = ENV['pcf_components']
 api_token         = ENV['app_admin_api_key']
 pcf_ert_domain    = ENV['pcf_ert_domain']
 pcf_opsman_admin  = ENV['pcf_opsman_admin']
@@ -69,5 +68,5 @@ opsman_url        = "https://opsman.#{pcf_ert_domain}"
 tiles_versions = get_deployed_products(opsman_url, pcf_opsman_admin, pcf_opsman_passwd)
 tiles_versions.each do |tile, version|
   tile_id = get_component_id_from_description(api_endpoint, api_token, tile)
-  update_component_version(api_endpoint, pcf_components, tile_id, tile, version)
+  update_component_version(api_endpoint, tile_id, tile, version)
 end
