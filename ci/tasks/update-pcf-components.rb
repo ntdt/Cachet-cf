1560193011741f0ab82a14e68793df6edf283f8e
Tho Duy Nguyen <tho.duy.nguyen@desjardins.com>
2017-07-27 16:10:16 -0400

    add map tile=>description

--- ci/tasks/update-pcf-components.rb
+++ ci/tasks/update-pcf-components.rb
@@ -21,16 +21,16 @@ def get_component_id_from_description(api_url, api_token, description)
   return 0
 end
 
-def update_component_version(api_url, api_token, component_id, description, version)
+def update_component_version(api_url, api_token, component_id, description, version, components)
   url = URI("#{api_url}/components/#{component_id}")
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = true
   request = Net::HTTP::Put.new(url)
   request['Content-Type'] = 'application/json'
   request['X-Cachet-Token'] = "#{api_token}"
-  puts "Update tile #{description} to #{version}"
-  name = "#{description} #{version}"
-  request.body = "{\"name\":\"#{name}\", \"status\":1}"
+  name = components[description]
+  puts "Update tile #{name} to #{version}"
+  request.body = "{\"name\":\"#{name} #{version}\", \"status\":1}"
   response = http.request(request)
   response.read_body
 end
@@ -64,9 +64,16 @@ pcf_opsman_admin  = ENV['pcf_opsman_admin']
 pcf_opsman_passwd = ENV['pcf_opsman_admin_passwd']
 api_endpoint      = "https://#{ENV['app_name']}.apps.#{pcf_ert_domain}/api/v1"
 opsman_url        = "https://opsman.#{pcf_ert_domain}"
+components        = {"cf" => "Pivotal Elastic Runtime",
+                     "p-mysql" => "MySQL for PCF",
+                     "p-redis" => "Redis for PCF",
+                     "p-rabbitmq" => "RabbitMQ for PCF",
+                     "p-spring-cloud-services" => "Spring Cloud Services",
+                     "Pivotal_Single_Sign-On_Service" => "Single Sign-On",
+                     "apm" => "PCF Metrics"}
 
 tiles = get_deployed_products(opsman_url, pcf_opsman_admin, pcf_opsman_passwd)
 tiles.each do |tile|
   tile_id = get_component_id_from_description(api_endpoint, api_token, tile['name'])
-  update_component_version(api_endpoint, api_token, tile_id, tile['name'], tile['version'])
+  update_component_version(api_endpoint, api_token, tile_id, tile['name'], tile['version'], components)
 end
